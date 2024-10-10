fc95276621dddcba147194fdaebff9393a501d62
Bruno Gusmão <brunogusmao43@gmail.com>
2024-10-10 00:43:39 -0300

    fixing bugs and wrapping the application with the userContext provider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import ChatApp from './components/ChatApp/ChatApp';
-import { UserProvider } from './UserContext';
+import { UserProvider, useUser, UserContext } from './UserContext';
 
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-    const { setUser } = setUser();
+    const { setUser } = useUser();
 
     const handleLogin = (username) => {
         setIsAuthenticated(true);
-        setUser(username);
+        setUser({username});
     };
 
   return (
-    <UserProvider>
       <Router>
         <Routes>
               <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-              <Route path="/chat" element={isAuthenticated ? <ChatApp /> : <ChatApp Navigate to="/login" />} />
+              <Route path="/chat" element={isAuthenticated ? <ChatApp /> : <Navigate to="/login" />} />
               <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
-    </UserProvider>
   );
 }
 
