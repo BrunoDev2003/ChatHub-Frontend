532bf0234928fc1d7a6815020f186c43af9c0abd
Bruno Gusmão <brunogusmao43@gmail.com>
2024-11-08 10:19:57 -0300

    defining the dark theme better

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -4,6 +4,7 @@ import { CssBaseline } from "@mui/material";
 
 export const theme = createTheme({
     palette: {
+        mode: 'light',
         primary: {
             light: '#dd33fa',
             main: '#d500f9',
@@ -17,7 +18,6 @@ export const theme = createTheme({
             dark: '#6d1b7b',
             contrastText: '#000',
         },
-        mode: 'light',
     },
 });
 
@@ -25,10 +25,33 @@ export const darkTheme = createTheme({
     palette: {
             mode: 'dark',
             primary: {
-                main: theme.palette.primary.dark_mode,
+                main: '#ff5252',
+                action: {
+                    active: '#fff',
+                    selected: (255, 255, 255, 0.16),
+                    disabledBackground: (255, 255, 255, 0.12),
+                    hover: (255, 255, 255, 0.08),
+                    disabled: (255, 255, 255, 0.3),
+                },
+                light: '#3d3636',
+                dark: '#121212',
+                text: '#fff',
+                contrastText: '#fff',
             },
             secondary: {
-                main: theme.palette.secondary.dark,
+                main: '#ff5252',
+                light: '#3d3636',
+                dark: '#545b5b',
+                text: (255, 255, 255, 0.7),
+                contrastText: '#fff',
             },
+            background: {
+                default: '#121212',
+                paper: '#121212',
+            },
+            divider: (255, 255, 255, 0.12),
+            disabled: {
+                text: (255, 255, 255, 0.5)
+            }
         },
     });
