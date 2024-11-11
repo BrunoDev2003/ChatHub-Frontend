e4c46ab7127809dd3d2a20e51fade1efe4311f5f
Bruno Gusmão <brunogusmao43@gmail.com>
2024-11-11 14:55:43 -0300

    improving the logic and style of the login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -4,11 +4,15 @@ import axiosInstance from '../../axiosInstance';
 import axios from 'axios';
 import { Container, Header, Form, Input, Button, Footer } from './LoginPage.styles';
 import { useUser } from '../../UserContext';
+import { Alert } from '@mui/material';
+import { CheckCircleOutline } from '@mui/icons-material';
 
 const LoginPage = ({ onLogin }) => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
   const navigate = useNavigate();
   const { setUser } = useUser();
 
@@ -30,10 +34,19 @@ const LoginPage = ({ onLogin }) => {
       console.log('Usuário logado com sucesso!', loggedUser);
       setUser({ id: loggedUser.id, username: loggedUser.username, isOnline: true});// setar userContext
       onLogin(loggedUser); // grava o userId e username logado
-      navigate('/chat');
+      setSuccess('Login bem-sucedido! Redirecionando...');
+      setError('');
+      setTimeout(() => navigate('/chat'), 2000); //redireciona para a rota /chat após 2 segundos;
     })
     .catch(error => {
-      console.error('Ocorreu um erro ao fazer login...', error)
+      if (error.response && error.response.status === 401) {
+        setError('Senha inválida!');
+        setSuccess('');
+      } else {
+        setError('Nome de usuário inválido!');
+        setSuccess('');
+      }
+      console.error('Ocorreu um erro ao fazer login...', error);
     });
     console.log('Username:', username);
     console.log('Password:', password);
@@ -42,6 +55,8 @@ const LoginPage = ({ onLogin }) => {
   return (
     <Container>
       <Header> ChatHub - Login</Header>
+      {error && <Alert iconMapping={{ success: <CheckCircleOutline fontSize="inherit"/>}} variant="filled" severity="error">{error}</Alert>}
+      {success && <Alert iconMapping={{ success: <CheckCircleOutline fontSize="inherit"/>}} variant="filled" severity="success">{success}</Alert>}
       <Form onSubmit={handleSubmit}>
         <Input type="text"
         placeholder="Nome de usuário" 
