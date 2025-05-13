e80eb0d9ba6b7a69584177c4bdbe5a4723229ce8
Bruno Gusmão <brunogusmao43@gmail.com>
2025-05-13 19:30:56 -0300

    updating axios to use the new custom domain url

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://www.chatshubapp.it.com/api',
     withCredentials: true,
 });
 
