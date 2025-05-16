cdb7ea009017d723c8a2782c3736c17707dd8c18
Bruno Gusmão <brunogusmao43@gmail.com>
2025-05-16 19:32:42 -0300

    fixing URL duplicates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,7 +28,7 @@ const Header = ({isUserOnline, messageListRef}) => {
 
     const handleLogout = async () => {
         try {
-            await axiosInstance.post(`https://${backendUrl}/api/logout`);
+            await axiosInstance.post(`/api/logout`);
             navigate('/login');
         } catch (error) {
             console.error('Erro ao fazer logout', error);
